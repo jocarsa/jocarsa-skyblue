29f439061ede90bc66719709b1106af3e1a48a70
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-03-26 20:03:07 +0100

    1

--- index.php
+++ index.php
@@ -2,24 +2,24 @@
 	include "inc/motorplantilla.php";
 	
 	
-	$json = file_get_contents('json/cabeza.json');
+	$json = file_get_contents('json/partes/cabeza.json');
 		$datos = json_decode($json, true);
 		$templateUrl = 'https://jocarsa.github.io/htmlcssjs/cabeza/cabeza.html';
 		echo renderTemplate($templateUrl, $datos);
 	
 	
 	if(isset($_GET['producto'])){
-		$json = file_get_contents('json/'.str_replace(" | ","_",$_GET['producto']).'.json');
+		$json = file_get_contents('json/productos/'.str_replace(" | ","_",$_GET['producto']).'.json');
 		$datos = json_decode($json, true);
 		$templateUrl = 'https://jocarsa.github.io/htmlcssjs/landing2/landing2.html';
 		echo renderTemplate($templateUrl, $datos);
 	}else if(isset($_GET['pagina'])){
-		$json = file_get_contents('json/'.$_GET['pagina'].'.json');
+		$json = file_get_contents('json/paginas/'.$_GET['pagina'].'.json');
 		$datos = json_decode($json, true);
 		$templateUrl = 'https://jocarsa.github.io/htmlcssjs/pagina2/pagina2.html';
 		echo renderTemplate($templateUrl, $datos);	
 	}else if(isset($_GET['categoria'])){
-		$json = file_get_contents('json/'.$_GET['categoria'].'.json');
+		$json = file_get_contents('json/categorias/'.$_GET['categoria'].'.json');
 		$datos = json_decode($json, true);
 		$templateUrl = 'https://jocarsa.github.io/htmlcssjs/rejilla2/rejilla2.html';
 		echo renderTemplate($templateUrl, $datos);	
@@ -30,7 +30,7 @@
 		echo renderTemplate($templateUrl, $datos);
 	}
 	
-	$json = file_get_contents('json/piedepagina.json');
+	$json = file_get_contents('json/partes/piedepagina.json');
 		$datos = json_decode($json, true);
 		$templateUrl = 'https://jocarsa.github.io/htmlcssjs/piedepagina/piedepagina.html';
 		echo renderTemplate($templateUrl, $datos);
